818e731665f11af362b42a79c4a3106c00747c5b
luisl0bao <luisfelipemartinslobao@gmail.com>
2026-04-20 08:34:56 +0100

    Delete dados.py

--- dados.py
+++
@@ -1,9 +0,0 @@
-clientes = {}   # { id: { "nome": ..., "data_nascimento": ..., "telefone": ..., "id_plano": ..., "data_inicio": ... } }
-planos = {}     # { id: (nome, num_treinos, preco_por_treino) }
-despesas = []   # [ (id, descricao, valor) ]
-
-proximo_id_plano = 1
-proximo_id_cliente = 1
-proximo_id_despesa = 1
-proximo_mes = 1
-saldo_acumulado = 0.0
